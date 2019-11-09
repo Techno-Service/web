ecead96b399fb9bd2b1609cbd0fd5376c065c8f0
Amr <muhammad.amro.moussa@gmail.com>
2019-11-09 13:50:53 +0200

    lh

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const defaults = {
   port: '8080',
   host: 'localhost',
-  baseURL: 'http://6080-dot-9728892-dot-devshell.appspot.com/api/'
+  baseURL: 'https://localhost:6080/api/'
 }
 module.exports = defaults
